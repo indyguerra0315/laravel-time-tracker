2a59466f3682079dffd9b435439ef1988d797baf
Indira Guerra Rodriguez <indyguerra0315@gmail.com>
2021-12-27 13:45:24 +0100

    feat: Add Task model and migration

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid as UuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    use UuidTrait;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'startTime',
+        'totalTime',
+        'isOpen',
+    ];
+}
